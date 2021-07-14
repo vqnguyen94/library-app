11d19a1417cb3190c678f18e842f45300c300625
Viet <vietnboi@gmail.com>
2021-07-14 10:11:25 -0500

    Added classes instead of constructors

--- script.js
+++ script.js
@@ -5,11 +5,20 @@
 // Multiple "pages" so that no scroll is needed to display a larger amount of books
 
 
-function Book(title, author, pages, read){
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+  }
 }
 
 function toggleStatus(book) {
